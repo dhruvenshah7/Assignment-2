ddf6f470f17add7c5ce92ca3adc45de60bac2152
Sunny <Sunny@DESKTOP-0A2K50N>
2016-01-28 04:42:09 -0500

    Student test Created

--- test/assignment2/StudentTest.java
+++ test/assignment2/StudentTest.java
@@ -153,14 +153,64 @@ public class StudentTest {
         assertEquals(expResult, result);
 
     }
-    
+
     @Test
-    public void testEqualsWithSameAndId() {
-        System.out.println("equals");
-        Object obj = null;
-        Student instance = new Student();
+    public void testEqualsWithSameNameAndId() {
+        System.out.println("testEqualsWithSameNameAndId");
+        boolean result = false;
+        Student obj = new Student("Dhruven", "2834", "M", 0.0);
+        Student instance = new Student("Dhruven", "2834", "M", 0.0);
         boolean expResult = true;
-        boolean result = instance.equals(obj);
+        if (instance.getName().equals(obj.getName()) && instance.getId().equals(obj.getId())) {
+
+            result = true;
+        }
+
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testEqualsWithSameNameAndDiffId() {
+        System.out.println("testEqualsWithSameNameAndDiffId");
+        boolean result = true;
+        Student obj = new Student("Dhruven", "2834", "M", 0.0);
+        Student instance = new Student("Dhruven", "1234", "M", 0.0);
+        boolean expResult = false;
+        if (instance.getName().equals(obj.getName()) && instance.getId() != obj.getId()) {
+
+            result = false;
+        }
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testEqualsWithDiffNameAndSameId() {
+        System.out.println("testEqualsWithDiffNameAndSameId");
+        boolean result = true;
+        Student obj = new Student("Dhruven", "2834", "M", 0.0);
+        Student instance = new Student("Sunny", "2834", "M", 0.0);
+        boolean expResult = false;
+        if (instance.getName() != obj.getName() && instance.getId() == obj.getId()) {
+
+            result = false;
+        }
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testEqualsWithDiffNameAndDiffId() {
+        System.out.println("testEqualsWithDiffNameAndSameId");
+        boolean result = true;
+        Student obj = new Student("Dhruven", "2834", "M", 0.0);
+        Student instance = new Student("Sunny", "1234", "M", 0.0);
+        boolean expResult = false;
+        if (instance.getName() != obj.getName() && instance.getId() != obj.getId()) {
+
+            result = false;
+        }
         assertEquals(expResult, result);
 
     }
